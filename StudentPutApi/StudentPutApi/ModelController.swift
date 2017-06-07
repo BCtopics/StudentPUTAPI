98b746cb0f1a78dd71cb7daa67250cf5b3f86dfe
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-07 10:28:37 -0600

    Add PUT Requests to modelController

--- StudentPutApi/StudentPutApi/ModelController.swift
+++ StudentPutApi/StudentPutApi/ModelController.swift
@@ -7,9 +7,41 @@
 //
 
 import Foundation
+import UIKit
 
 class StudentController {
     
+    static let baseURL = URL(string: "https://studentputapi-3a581.firebaseio.com/students")
     
+    static func putStudentWith(name: String, completion: @escaping (_ success: Bool) -> Void) {
+        
+        var success = false
+        defer { completion(success) }
+        
+        let student = Student(name: name)
+        
+        guard let baseURL = baseURL else { return }
+        let newEndPoint = baseURL.appendingPathComponent(UUID().uuidString)
+        
+        NetworkController.performRequest(for: newEndPoint, httpMethod: .put, urlParameters: nil, body: student.jsonData) { (data, error) in
+            
+            if let error = error {
+                NSLog("Error \(error)")
+            }
+
+            guard let data = data, let responseDataString = String(data: data, encoding: .utf8) else { return }
+            
+            if responseDataString.contains("error") {
+                print("Error: \(responseDataString)")
+            } else {
+                print("Successfully put student \(student.name) at endpoint. \n\(responseDataString))")
+            }
+            
+            
+        }
+        
+        
+        
+    }
     
 }
